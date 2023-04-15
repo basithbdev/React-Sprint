2b65d0873a7c9373320aa6402c939e48f79bc3ea
basithbdev <125488164+basithbdev@users.noreply.github.com>
2023-04-15 14:13:09 +0530

    feat: adds handler functions to receive input

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,40 @@
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = function () {
+
+    const [enteredTitle, setEnteredTitle] = useState("");
+    const [enteredAmount, setEnteredAmount] = useState("");
+    const [enteredDate, setEnteredDate] = useState("");
+
+    const titleChangeHandler = function (event) {
+        setEnteredTitle(event.target.value);
+    };
+
+    const amountChangeHandler = function (event) {
+        setEnteredAmount(event.target.value);
+    };
+
+    const dateChangeHandler = function (event) {
+        setEnteredDate(event.target.value);
+    }
+
     return (
         <form>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" />
+                    <input type="text" onChange={titleChangeHandler} />
                 </div>
 
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" />
+                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
                 </div>
 
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2023-01-01" max="2024-01-01" />
+                    <input type="date" min="2023-01-01" max="2024-01-01" onChange={dateChangeHandler} />
                 </div>
             </div>
 
