477d8899f75d51c8c7ebd28864ddb92d83b9687e
basithbdev <125488164+basithbdev@users.noreply.github.com>
2023-04-15 17:53:34 +0530

    feat: add 2-way binding to clear existing user i/p

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -29,6 +29,9 @@ const ExpenseForm = function () {
         };
 
         console.log(newExpenseData);
+        setEnteredTitle("");
+        setEnteredAmount("");
+        setEnteredDate("");
     }
 
     return (
@@ -36,17 +39,18 @@ const ExpenseForm = function () {
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler} />
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
                 </div>
 
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+                    <input type="number" min="0.01" step="0.01"
+                        value={enteredAmount} onChange={amountChangeHandler} />
                 </div>
 
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2023-01-01" max="2024-01-01" onChange={dateChangeHandler} />
+                    <input type="date" min="2023-01-01" max="2024-01-01" value={enteredDate} onChange={dateChangeHandler} />
                 </div>
             </div>
 
