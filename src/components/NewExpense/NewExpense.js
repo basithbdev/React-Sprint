10454211f32a59fbaeb79872ef1f658fb2dbcbe0
basithbdev <125488164+basithbdev@users.noreply.github.com>
2023-04-15 13:30:43 +0530

    feat: creates NewExpense component

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,12 @@
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+
+const NewExpense = function () {
+    return (
+        <div className="new-expense">
+            <ExpenseForm />
+        </div>
+    )
+};
+
+export default NewExpense;
